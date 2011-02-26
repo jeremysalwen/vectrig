0663fb10e5f9d0e6d9f2a263d372bbf1fd32d3dd
Jeremy Salwen <jeremysalwen@gmail.com>
2011-02-26 06:45:39 -0500

    Lots of changes, stats file added.

--- main.c
+++ main.c
@@ -4,81 +4,112 @@
 #include <stdint.h>
 
 //#define CLOCK
-#define SIZE (int)(512*3.14159)
+#define SIZE (int)(1024*3.14159)
 
-#define QUARTPI 0.785398163
-#define HALFPI 1.570796327
+#define FLTEND(x) x##d
+#define FLTTYPE double
+#define QUARTPI  FLTEND(0.785398163)
+#define HALFPI  FLTEND(1.570796327)
 
-#define ROOT2O2 0.707106781
+#define ROOT2O2  FLTEND(0.707106781)
 
-#define SUP7ORD1 0.99999748721912
-#define SUP7ORD3 -0.16665168108677
-#define SUP7ORD5 0.0083095170074559
-#define SUP7ORD7 -1.8447222287032252e-4
+#define SUP7ORD3 FLTEND(-0.16665168108677)
+#define SUP7ORD5  FLTEND(0.0083095170074559)
+#define SUP7ORD7  FLTEND(-1.8447222287032252e-4)
 
-static void sinsuperseries7(float* __restrict__ arr,int len) {
+void sinsuperseries7(float* __restrict__ arr,int len) {
    for(int i=0; i<len; i++) {
-      float tmp=arr[i]*arr[i];
-      arr[i]=arr[i]*SUP7ORD1+arr[i]*tmp*SUP7ORD3 +arr[i]*tmp*tmp*SUP7ORD5+arr[i]*tmp*tmp*tmp*SUP7ORD7;
+      FLTTYPE tmp=arr[i]*arr[i];
+      arr[i]+=arr[i]*tmp*(SUP7ORD3 + tmp*(SUP7ORD5+tmp*SUP7ORD7));
    }
 }
 
 
-#define SUP9ORD9 2.5984422633634844e-6
-#define SUP9ORD7 -1.9804930374010013e-4
-#define SUP9ORD5 0.0083329670804495
-#define SUP9ORD3 -0.1666665171729
-#define SUP9ORD1 0.99999998312198
+#define SUP9ORD9  FLTEND(2.5984422633634844e-6)
+#define SUP9ORD7  FLTEND(-1.9804930374010013e-4)
+#define SUP9ORD5  FLTEND(0.0083329670804495)
+#define SUP9ORD3  FLTEND(-0.1666665171729)
+#define SUP9ORD1  FLTEND(0.99999998312198)
 
-static void sinsuperseries9(float* __restrict__ arr,int len) {
+void sinsuperseries9(float* __restrict__ arr,int len) {
    for(int i=0; i<len; i++) {
-      float sqr=arr[i]*arr[i];
+      FLTTYPE sqr=arr[i]*arr[i];
       arr[i]+=sqr*arr[i]*(SUP9ORD3+sqr*(SUP9ORD5+sqr*(SUP9ORD7+sqr*SUP9ORD9)));
    }
 }
 
-#define PIO6f 0.523598776f
+#define PIO6  FLTEND(0.523598776)
 
-#define SA5f 2.74201854577e-06f
-#define SA4f -1.98410347969e-04f
-#define SA3f 8.33333320429e-03f
-#define SA2f -1.66666666666e-01f
+#define SA5  FLTEND(2.74201854577e-06)
+#define SA4  FLTEND(-1.98410347969e-04)
+#define SA3  FLTEND(8.33333320429e-03)
+#define SA2  FLTEND(-1.66666666666e-01)
 
-static void bestseries9div3(float * __restrict__ arr,int len) {
+void bestseries9div3(float * __restrict__ arr,int len) {
    for(int i=0; i<len; i++) {
-      float or=arr[i];
-      if(or>PIO6f) {
+      FLTTYPE or=arr[i];
+      if(or>PIO6) {
         arr[i]/=3;
       }
-      float sqr=arr[i]*arr[i];
-      arr[i]+=arr[i]*sqr*(SA2f+sqr*(SA3f+sqr*(SA4f+SA5f*sqr))); 
-     arr[i]+=(or>PIO6f)*arr[i]*(2-4*arr[i]*arr[i]);
+      FLTTYPE sqr=arr[i]*arr[i];
+      arr[i]+=arr[i]*sqr*(SA2+sqr*(SA3+sqr*(SA4+SA5*sqr))); 
+     arr[i]+=(or>PIO6)*arr[i]*(2-4*arr[i]*arr[i]);
    }
 }
-#define MMS99f 2.6199025197195120855e-6f
-#define MMS97f -0.00019816322019412579628f
-#define MMS95f 0.0083331628953425015333f
-#define MMS93f -0.16666663516396446563f
+#define MMS99  FLTEND(2.6199025197195120855e-6)
+#define MMS97  FLTEND(-0.00019816322019412579628)
+#define MMS95  FLTEND(0.0083331628953425015333)
+#define MMS93  FLTEND(-0.16666663516396446563)
 
-static void minimaxseries9(float* __restrict__ arr, int len) {
+void minimaxseries9(float* __restrict__ arr, int len) {
   for(int i=0; i<len; i++) {
-    float sqr=arr[i]*arr[i];
-    arr[i]+=sqr*arr[i]*(MMS93f + sqr*(MMS95f + sqr* (MMS97f + sqr*MMS99f)));
+    FLTTYPE sqr=arr[i]*arr[i];
+    arr[i]+=sqr*arr[i]*(MMS93 + sqr*(MMS95 + sqr* (MMS97 + sqr*MMS99)));
   }
 }
-#define MMS1111f -2.3984563163388346477e-8f
-#define MMS119f 2.7531152930120359229e-6f
-#define MMS117f -0.00019840988242599135899f
-#define MMS115f 0.0083333320944743463687f
-#define MMS113f -0.16666666651391896548f
+#define MMS1111  FLTEND(-2.3984563163388346477e-8)
+#define MMS119  FLTEND(2.7531152930120359229e-6)
+#define MMS117  FLTEND(-0.00019840988242599135899)
+#define MMS115  FLTEND(0.0083333320944743463687)
+#define MMS113  FLTEND(-0.16666666651391896548)
 
-static void minimaxseries11(float* __restrict__ arr, int len) {
+ void minimaxseries11(float* __restrict__ arr, int len) {
   for(int i=0; i<len; i++) {
-    float sqr=arr[i]*arr[i];
-    arr[i]+=sqr*arr[i]*(MMS113f + sqr*(MMS115f + sqr* (MMS117f + sqr*(MMS119f+sqr*MMS1111f))));
+    FLTTYPE sqr=arr[i]*arr[i];
+    arr[i]+=sqr*arr[i]*(MMS113 + sqr*(MMS115 + sqr* (MMS117 + sqr*(MMS119+sqr*MMS1111))));
   }
 }
-static void sinseries05(float* __restrict__ arr, int len) {
+
+#define MMS1313  FLTEND(1.5456812420974751789e-10)
+#define MMS1311  FLTEND(-2.5033643691216283785e-8)
+#define MMS139  FLTEND(2.7557046632657704673e-6)
+#define MMS137  FLTEND(-0.00019841267882522861091)
+#define MMS135  FLTEND(0.0083333333272969841645)
+#define MMS133  FLTEND(-0.16666666666613490804)
+
+void minimaxseries13(float* __restrict__ arr, int len) {
+  for(int i=0; i<len; i++) {
+    int x=(int)(arr[i]/HALFPI);
+    arr[i]-=x*HALFPI;
+    FLTTYPE sqr=arr[i]*arr[i];
+    arr[i]+=sqr*arr[i]*(MMS133 + sqr*(MMS135 + sqr* (MMS137 + sqr*(MMS139 + sqr*(MMS1311 + sqr * MMS1313)))));
+  }
+}
+#define MMS1515  FLTEND(7.3912059649841211983e-13)
+#define MMS1513  FLTEND(1.6049620931934533410e-10)
+#define MMS1511  FLTEND(-2.5051931236723817451e-8)
+#define MMS159  FLTEND(2.7557317438440821200e-6)
+#define MMS157  FLTEND(-0.00019841269832025835003)
+#define MMS155  FLTEND(0.0083333333333122242661)
+#define MMS153  FLTEND(-0.16666666666666527182)
+
+void minimaxseries15(float* __restrict__ arr, int len) {
+  for(int i=0; i<len; i++) {
+    FLTTYPE sqr=arr[i]*arr[i];
+     arr[i]+=sqr*arr[i]*(MMS133 + sqr*(MMS135 + sqr* (MMS137 + sqr*(MMS139 + sqr*(MMS1311 + sqr * (MMS1313+sqr*MMS1515))))));
+  }
+}
+ void sinseries05(float* __restrict__ arr, int len) {
     float  orig[len];
     float  tmp[len];
     float lol;
@@ -115,7 +146,7 @@ static void sinseries05(float* __restrict__ arr, int len) {
      arr[i]-=tmp[i];
    }
 }
-static void sinseries0(float* __restrict__ arr, int len) {
+ void sinseries0(float* __restrict__ arr, int len) {
     float  orig[len];
     float  tmp[len];
     float lol;
@@ -151,96 +182,54 @@ static void sinseries0(float* __restrict__ arr, int len) {
       arr[i]+=tmp[i];
     }*/
 }
-static void vecsin(float* arr, int size) {
+ void vecsin(float* arr, int size) {
    for(int i=0; i<size; i++) {
      arr[i]=sin(arr[i]);
    }
 }
-int main() {
-   float sinarr[SIZE];
-   float sin0arr[SIZE];
-   float sin05arr[SIZE];
-   float supsin7arr[SIZE];
-   float supsin9arr[SIZE];
-   float bestseries9div3arr[SIZE];
-   float mms9arr[SIZE];
-   float mms11arr[SIZE];
+static void evaluateFunction(const char* name, void (*func)(float*,int) ) {
+   float arr[SIZE];
+   float vecsinarr[SIZE];
    for(int i=0; i<SIZE; i++) {
-     sinarr[i]=(float)i/1024;
-     sin0arr[i]=sinarr[i];
-     sin05arr[i]=sinarr[i];
-     supsin7arr[i]=sinarr[i];
-     supsin9arr[i]=sinarr[i];
-     bestseries9div3arr[i]=sinarr[i];
-     mms9arr[i]=sinarr[i];
-     mms11arr[i]=sinarr[i];
+     arr[i]=(float)i/2048;
+     vecsinarr[i]=arr[i];
    }
+   vecsin(vecsinarr,SIZE);
 #ifdef CLOCK
    clock_t endwait=clock();
    for(int i=0; i<100000; i++) {
 #endif
-      sinseries0(sin0arr,SIZE);
+      func(arr,SIZE);
 #ifdef CLOCK
    }
-
-  printf("diff sin0: %d\n",clock()-endwait);
-   endwait=clock();
-   for(int i=0; i<100000; i++) {
+   printf("time for %s: %d\n",name,clock()-endwait);
 #endif
-     vecsin(sinarr,SIZE);
-#ifdef CLOCK
+   unsigned int off=0;
+   float maxerr=0;
+   float maxrelerr=0;
+   for(int i=0; i<SIZE; i++) {
+     if(arr[i]!=vecsinarr[i]) {
+        off++;
+      }
+     if(fabs(arr[i]-sin((float)i/2048)) >maxerr) {
+       maxerr=fabs(arr[i]-sin((float)i/2048));
+     }
+     if(arr[i]!=0 && fabs((arr[i]-sin((float)i/2048))/arr[i]) >maxrelerr) {
+       maxrelerr=fabs((arr[i]-sin((float)i/2048))/arr[i]);
+     }
    }
-  printf("diff vecsin: %d\n",clock()-endwait);
+   printf("Evaluation for %s: off:%d maxerr:%10.10fe-7 maxrelerr:%10.10fe-7\n",name,off, maxerr*10e7, maxrelerr*10e7);
+}
 
-    endwait=clock();
-   for(int i=0; i<100000; i++) {
-#endif
-
-     sinseries05(sin05arr,SIZE);
-#ifdef CLOCK
-
-   }
-  printf("diff sin05: %d\n",clock()-endwait);
-
-    endwait=clock();
-   for(int i=0; i<100000; i++) {
-#endif
-     sinsuperseries7(supsin7arr,SIZE);
-#ifdef CLOCK
-
-   }
-  printf("diff supsin7: %d\n",clock()-endwait);
-    endwait=clock();
-   for(int i=0; i<100000; i++) {
-#endif
-     sinsuperseries9(supsin9arr,SIZE);
-#ifdef CLOCK
-   }
-  printf("diff supsin9: %d\n",clock()-endwait);
-  endwait=clock();
-  for(int i=0; i<100000; i++) {
-#endif
-     bestseries9div3(bestseries9div3arr,SIZE);
-#ifdef CLOCK
-  }
-  printf("diff bestseries9div3: %d\n",clock()-endwait);
-  endwait=clock();
-  for(int i=0; i<100000; i++) {
-#endif
-     minimaxseries9(mms9arr,SIZE);
-#ifdef CLOCK
-  }
-  printf("diff mms9: %d\n",clock()-endwait);
-  endwait=clock();
-  for(int i=0; i<100000; i++) {
-#endif
-     minimaxseries11(mms11arr,SIZE);
-#ifdef CLOCK
-  }
-  printf("diff mms11: %d\n",clock()-endwait);
-#endif
-  printf("#\tX\tSIN\tSIN0\tSIN05\tSUPSIN7\tSUPSIN9\tBESTSERIES\n"); 
-  for(int i=0; i<SIZE; i++) {
-    printf("%f %f %f %f %f %f %f %f %d\n",(float)i/1024,sinarr[i],sin0arr[i],supsin7arr[i],supsin9arr[i],bestseries9div3arr[i],mms9arr[i],mms11arr[i],mms11arr[i]==sinarr[i]);
-  }
+int main() {
+evaluateFunction("vecsin",&vecsin);
+evaluateFunction("sinseries0",&sinseries0);
+evaluateFunction("sinseries05",&sinseries05);
+evaluateFunction("supersin7",&sinsuperseries7);
+evaluateFunction("supersin9",&sinsuperseries9);
+evaluateFunction("best9div3",&bestseries9div3);
+evaluateFunction("minimaxseries9",&minimaxseries9);
+evaluateFunction("minimaxseries11",&minimaxseries11);
+evaluateFunction("minimaxseries13",&minimaxseries13);
+evaluateFunction("minimaxseries15",&minimaxseries15);
 }
