a151ee49f85c4f61c45ff83b8ef5416d90a7af1d
Jeremy Salwen <jeremysalwen@gmail.com>
2011-03-31 22:59:46 -0400

    Added b9 and b11 series.  They are the the polynomials created by projecteting sin(x)-x onto the set [x^3,x^5,x^7,x^9](+x^11) with the L^2 norm.
    The b9 series in particular appears to be more accurate than any other series I've gotten.

--- main.c
+++ main.c
@@ -192,3 +192,30 @@ void gnuseries(float* __restrict__ arr, int len) {
       arr[i]+= arr[i]*sqr*(GNUS1+sqr*(GNUS2 +sqr*(GNUS3 +sqr*(GNUS4+sqr*(GNUS5+sqr*GNUS6)))));
     }
 }
+FLTTYPE B91  = FLTEND(-1.6666658777408987333e-1);
+FLTTYPE B92  = FLTEND(8.3330585579660222884e-3);
+FLTTYPE B93  = FLTEND(-1.9809580202098392708e-4);
+FLTTYPE B94  = FLTEND( 2.6065544878032086552e-6);
+
+void b9series(float* __restrict__ arr, int len) {
+   bringtorange(arr,len);
+   FLTTYPE sqr;
+    for(int i=0; i<len; i++) {
+      sqr=arr[i]*arr[i];
+      arr[i]+= arr[i]*sqr*(B91+sqr*(B92 +sqr*(B93 +sqr*B94)));
+    }
+}
+FLTTYPE B111  = FLTEND(-1.6666666621113970589e-1);
+FLTTYPE B112  = FLTEND(8.3333309585278954692e-3);
+FLTTYPE B113  = FLTEND(-1.9840861674155877174e-4);
+FLTTYPE B114  = FLTEND(2.7525172542458407497e-6);
+FLTTYPE B115  = FLTEND(-2.3897606931720883012e-8);
+
+void b11series(float* __restrict__ arr, int len) {
+   bringtorange(arr,len);
+   FLTTYPE sqr;
+    for(int i=0; i<len; i++) {
+      sqr=arr[i]*arr[i];
+      arr[i]+= arr[i]*sqr*(B111+sqr*(B112 +sqr*(B113 +sqr*(B114+sqr*B115))));
+    }
+}
